3c797afcb5a892c15a09031cc907c9af66c6c15a
Mustafa Aydemir <mustafa@mustafa-aydemir.europe-west3-c.c.centering-rex-237206.internal>
2020-04-24 17:23:40 +0000

    Initial commit

--- main.sh
+++ main.sh
@@ -76,6 +76,9 @@ else
     # Backup
     if [ $1 == "backup" ]; then
 
+        # MacOS : Confirm
+        [ "$OSTYPE" == "darwin"* ] && osascript -e 'tell app "System Events" to display dialog "Do you want to back up your computer?"'
+
         # Clear backup folder
         [ ! -d /.criexe/dropbox-backup/backup/ ] && sudo rm -R /.criexe/dropbox-backup/backup/
         sudo mkdir -p /.criexe/dropbox-backup/backup/
@@ -87,6 +90,9 @@ else
         sudo criexe-dropbox upload \
             /.criexe/dropbox-backup/backup/* \
             /$(date +"%Y-%m-%d")/
+
+        # Done : MacOS
+        [ "$OSTYPE" == "darwin"* ] && sudo osascript -e 'display notification "Completed!" with title "Criexe - Dropbox Backup"'
             
     fi
 
