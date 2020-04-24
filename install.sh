409934ea4f9d105bfa1c355c2291fff4d2224137
Mustafa Aydemir <mustafa@mustafa-aydemir.europe-west3-c.c.centering-rex-237206.internal>
2020-04-24 16:25:02 +0000

    Initial commit

--- install.sh
+++ install.sh
@@ -8,14 +8,18 @@ sudo mkdir -p /.criexe/dropbox-backup
 sudo mkdir -p /.criexe/dropbox-backup/hooks
 sudo mkdir -p /.criexe/dropbox-backup/backups
 
-# Download
-sudo curl -sSL https://raw.githubusercontent.com/criexe/dropbox-backup/master/backup.sh -o /usr/local/bin/criexe-dropbox-backup
-sudo curl -sSL https://raw.githubusercontent.com/criexe/dropbox-backup/master/hooks/before-backup.sh -o /.criexe/dropbox-backup/hooks/before-backup.sh
-sudo curl -sSL https://raw.githubusercontent.com/criexe/dropbox-backup/master/hooks/after-backup.sh -o /.criexe/dropbox-backup/hooks/after-backup.sh
-
-# Permissions
+# Download Backup Script
+sudo curl -sSL https://raw.githubusercontent.com/criexe/dropbox-backup/master/backup.sh -o /usr/local/bin/criexe-dropbox-backup && \
 sudo chmod a+x /usr/local/bin/criexe-dropbox-backup
+
+# Hook: Before Backup
+[ ! -f /.criexe/dropbox-backup/hooks/before-backup.sh ] && \
+sudo curl -sSL https://raw.githubusercontent.com/criexe/dropbox-backup/master/hooks/before-backup.sh -o /.criexe/dropbox-backup/hooks/before-backup.sh && \
 sudo chmod a+x /.criexe/dropbox-backup/hooks/before-backup.sh
+
+# Hook: Before Backup
+[ ! -f /.criexe/dropbox-backup/hooks/after-backup.sh ] && \
+sudo curl -sSL https://raw.githubusercontent.com/criexe/dropbox-backup/master/hooks/after-backup.sh -o /.criexe/dropbox-backup/hooks/after-backup.sh && \
 sudo chmod a+x /.criexe/dropbox-backup/hooks/after-backup.sh
 
 echo "Done!";
